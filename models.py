45dab762f34a60de582fbfe78338b813a4c89e77
prajwal-nivangune <prajwal.nivangune@mindstix.com>
2025-08-07 22:28:04 +0530

    fix: minor issue resolved

--- models.py
+++ models.py
@@ -5,19 +5,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
-class Role(enum.Enum):
-    ADMIN = "admin"
-    DOCTOR = "doctor"
-    MEMBER = "member"
-
-
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(300), nullable=False)
-    role = db.Column(Enum(Role), nullable=False)
+    role = db.Column(db.String(10), nullable=False)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
